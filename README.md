0579bcbb2217b0c117dfb9d54904b97c5c06dc81
Darío Javier Cravero <dario@uxtemple.com>
2017-09-25 19:10:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# KeyboardAvoidingAndDismissingView
+
+This component wraps `KeyboardAvoidingView` in a `TouchableWithoutFeedback` that
+only gets enabled when the keyboard is shown.
+
+It takes the same props as the [KeyboardAvoidingView](https://facebook.github.io/react-native/docs/keyboardavoidingview.html).
+
+License MIT.
+
+By ViewsDX :).
---
+++ index.js
@@ -0,0 +1,54 @@
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from 'react-native'
+import React from 'react'
+
+export default class KeyboardAvoidingAndDismissingView extends React.PureComponent {
+  static propTypes = KeyboardAvoidingView.propTypes
+
+  static defaultProps = {
+    keyboardVerticalOffset: 0,
+    style: { flex: 1 },
+  }
+
+  state = {
+    isDisabled: true,
+  }
+
+  componentWillMount() {
+    this.hideListener = Keyboard.addListener('keyboardDidHide', this.onHide)
+    this.showListener = Keyboard.addListener('keyboardDidShow', this.onShow)
+  }
+
+  componentWillUnmount() {
+    this.hideListener.remove()
+    this.showListener.remove()
+  }
+
+  onHide = () => {
+    this.setState({
+      isDisabled: true,
+    })
+  }
+
+  onShow = () => {
+    this.setState({
+      isDisabled: false,
+    })
+  }
+
+  render() {
+    const { props, state } = this
+
+    return (
+      <TouchableWithoutFeedback
+        onPressOut={Keyboard.dismiss}
+        disabled={state.isDisabled}
+      >
+        <KeyboardAvoidingView {...props}>{props.children}</KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    )
+  }
+}
